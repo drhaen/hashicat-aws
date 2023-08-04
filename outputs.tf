765095575d3bdeaa38d7620d621802d2e4d8b570
drhaen <dhaennelt@yahoo.com>
2023-08-04 20:23:42 +0000

    Added remote backend

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,19 @@ output "catapp_url" {
 output "catapp_ip" {
   value = "http://${aws_eip.hashicat.public_ip}"
 }
+
+# data sources
+#data "aws_caller_identity" "inuse" {}
+
+# outputs
+# output "account_id" {
+#   value = data.aws_caller_identity.inuse.account_id
+# }
+
+# output "caller_arn" {
+#   value = data.aws_caller_identity.inuse.arn
+# }
+
+# output "caller_user" {
+#   value = data.aws_caller_identity.inuse.user_id
+# }
